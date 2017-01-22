b6b34f765430b4f616f6321eba0620876890fe42
morkar <mahmoud.norman.amr@gmail.com>
2017-01-23 00:57:51 +0100

    enter the commit message for your changes. Lines starting
    code snippets for the first two sensor implementations

--- home-o-matic.py
+++ home-o-matic.py
@@ -1,1 +1,11 @@
 #this will be the home-o-matic master
+
+
+def main():
+    #initialize database
+
+    #initialize gascounter
+    GasCounter gasCounter(gasDAO)
+
+    #initialize weather sensor
+    OpenWeather openWeather(weatherDAO)
