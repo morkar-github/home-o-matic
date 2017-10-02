ad54c37c3d044463fe308e747370c55de88ed6c6
morkar <mahmoud.norman.amr@gmail.com>
2017-10-02 23:53:29 +0200

    some output so we see the data on the console

--- Weather.py
+++ Weather.py
@@ -1,16 +1,17 @@
 class Weather:
     def __init__(self, updatesPerHour):
         self._updatesPerHour = updatesPerHour
-            self._sensor = OpenWeather('abcc6a6d692594e4c9524d7088d5184b')
-                self._dao = WeatherDAO()
+        self._sensor = OpenWeather('abcc6a6d692594e4c9524d7088d5184b')
+        self._dao = WeatherDAO()
         #nothing yet
         
         def loop(self):
             while(1 == 1):
                 try:
                     t, t_min, t_max = self._sensor.getTemperature()
-                        self._dao.saveTemperature(t, t_min, t_max)
-                        time.sleep(60*(60/self._updatesPerHour)) # get values twice per hour
+                    print "Temperature: " + t + " min: " + t_min + " max: " + t_max
+                    self._dao.saveTemperature(t, t_min, t_max)
+                    time.sleep(60*(60/self._updatesPerHour)) # get values twice per hour
                 except Exception as unknownException:
                     print unknownException.args
-                        print unknownException	
+                    print unknownException
