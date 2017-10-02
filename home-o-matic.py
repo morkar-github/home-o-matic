8158ac9c0b245117c0aa559c10adc385de4d6908
morkar <mahmoud.norman.amr@gmail.com>
2017-10-02 23:56:53 +0200

    im trying to get the example for openweather working

--- home-o-matic.py
+++ home-o-matic.py
@@ -1,9 +1,11 @@
+import Weather
+
 #this will be the home-o-matic master
 def __main__(self):
 	#gas
     #GasCounter gasCounter(counterValue)
 	
 	#weather
-	Weather weather(3)
+    weather = Weather(3)
 
 
